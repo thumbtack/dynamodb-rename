fb18cad2989302b8ba9181509fb4cab36c78f4dd
Marco Almeida <mfa@thumbtack.com>
2018-04-10 14:20:03 -0700

    Transfer ownership

--- main.go
+++ main.go
@@ -1,21 +1,17 @@
 /*
-   Copyright (C) 2018  Marco Almeida <marcoafalmeida@gmail.com>
+ Copyright 2018 Thumbtack, Inc.
 
-   This file is part of dynamodb-rename.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-   dynamodb-rename is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
+         http://www.apache.org/licenses/LICENSE-2.0
 
-   dynamodb-rename is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License along
-   with this program; if not, write to the Free Software Foundation, Inc.,
-   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 */
 
 package main
@@ -38,7 +34,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodbstreams"
-	"github.com/marcoalmeida/ratelimiter"
+	"github.com/thumbtack/goratelimit"
 )
 
 const (
